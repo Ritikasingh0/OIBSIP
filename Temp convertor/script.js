5127d38d288ef1551e7a678494538fe96ef7c7f7
Ritika kumari <98579242+Ritikasingh0@users.noreply.github.com>
2023-03-28 08:38:17 +0530

    Add files via upload

---
+++ Temp convertor/script.js
@@ -0,0 +1,4 @@
+var celsius = document.getElementById("celsius").value;
+var fahrenheit = (celsius * 9/5) + 32;
+document.getElementById("result").innerHTML = celsius + " degrees Celsius is " + fahrenheit + " degrees Fahrenheit.";
+
